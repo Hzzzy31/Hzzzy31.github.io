42d5dd38e8f95da2d337dbd477007aa224fe17ec
贺镇宇 <320220940281@lzu.edu.cn>
2024-07-12 04:04:17 +0800

    hzzzy-更新三创

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,7 +4,7 @@ const siteMetadata = {
   author: 'Eason He',
   headerTitle: 'Hzzzy Blog',
   description: '一个CS专业在读的CV爱好者的个人博客',
-  language: 'zh-cn',
+  language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
